46875f0d3dae805132a8b5e552d42cd0220f8968
Daniel <danielmsdiaz@hotmail.com>
2022-12-20 01:39:26 -0300

    finalizando as requisicoes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,3 +11,55 @@ export const createUser = async (req: Request, res: Response) => {
     const user = await User.create({name, born_date, email});
     res.json({Usuario: user});
 }
+
+export const listAllUsers = async (req: Request, res: Response) => {
+    const users: UserInstance[] = await User.findAll();
+    res.json({Usuarios: users});
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    const user: UserInstance | null = await User.findByPk(id);
+    if(user){
+        res.json({Usuario: user});
+    }
+    else{
+        res.json({Erro: "usuário não encontrado"});
+    }
+}
+
+export const deleteUserById = async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    const user: UserInstance | null = await User.findByPk(id);
+    if(user){
+        await User.destroy({where: {id: id}});
+        res.json({Aviso: "Usuário de id: " + id + " removido com sucesso!"});
+    }
+    else{
+        res.json({Erro: "usuário não encontrado"});
+    }
+}
+
+export const UpdateUserById = async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    const body = req.body;
+    const user: UserInstance | null = await User.findByPk(id);
+
+    if(user){
+        if(body.name){
+            user.name = body.name;
+        }
+        if(body.born_date){
+            user.born_date = body.born_date;
+        }
+        if(body.email){
+            user.email = body.email;
+        }
+        await user.save();
+        res.json({Sucesso: "usuário atualizado!"});
+    }
+    else{
+        res.json({Erro: "usuário não encontrado"});
+    }
+}
+
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -5,5 +5,9 @@ const router = Router();
 
 router.get("/ping", userController.pong);
 router.post("/user", userController.createUser);
+router.get("/user", userController.listAllUsers);
+router.get("/user/:id", userController.getUserById);
+router.delete("/user/:id", userController.deleteUserById);
+router.put("/user/:id", userController.UpdateUserById);
 
 export default router;
